24d951bf7f1766fed579d72e1c84d2ccc88057ee
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2024-07-25 13:15:40 -0300

    unidade1

--- unidade1/1_feature_extraction/2_atributes_extraction.py
+++ unidade1/1_feature_extraction/2_atributes_extraction.py
@@ -1,5 +1,7 @@
 # Atividade
-# Um função que retorne a quantidade de sentenças em um texto.
-# Uma função que retorne a quantidade de palavras que começam com letra maiúscula em um texto.
-# Uma função que retorne a quantidade de caracteres numéricos em um texto.
-# Uma função que retorne a quantidade de palavras que estão  em caixa alta.
+# 1) Um função que retorne a quantidade de sentenças em um texto.
+# 2) Uma função que retorne a quantidade de palavras que começam com letra maiúscula em um texto.
+# 3) Uma função que retorne a quantidade de caracteres numéricos em um texto.
+# 4) Uma função que retorne a quantidade de palavras que estão  em caixa alta.
+# 5) Crie um dataframe de 1 coluna e 4 linhas. Este contém os textos testados nas questões anteriores.
+# 6) Crie e print um dataframe de 4 linhas e 4 colunas e preencha com a execução das funções das questões 1,2,3 e 4.  Cada coluna deve conter os resultados das funções. Utilize a função apply() no dataframe criado na questão 5.
