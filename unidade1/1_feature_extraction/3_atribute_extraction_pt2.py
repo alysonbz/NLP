be1ce483be129896e9c5656d6b6fb2dd715e2eb8
jparxd <jparxd@gmail.com>
2024-07-25 14:27:23 -0300

    atvidades 25/07

--- unidade1/1_feature_extraction/3_atribute_extraction_pt2.py
+++ unidade1/1_feature_extraction/3_atribute_extraction_pt2.py
@@ -1,55 +1,60 @@
 from src.utils import load_pfizer_tweet_dataset
-
-tweets = load_pfizer_tweet_dataset()
 import matplotlib.pyplot as plt
 
-## WORD COUNT
+# Carregar o conjunto de dados de tweets do Pfizer
+tweets = load_pfizer_tweet_dataset()
 
+
+## WORD COUNT
 
 # a) Function that returns number of words in a string
 def count_words(string):
     # Split the string into words
-    words = ____.____
+    words = string.split()
 
     # Return the number of words
-    return ____(____)
+    return len(words)
 
 
 # Create a new feature word_count
-tweets['word_count'] = tweets[____].apply(____)
+tweets['word_count'] = tweets['text'].apply(count_words)
 
-# Print the average word count of the talks
-print("Word count mean: ", tweets[____].____)
+# Print the average word count of the tweets
+print("Word count mean: ", tweets['word_count'].mean())
 
 ## CHAR COUNT
 
 # Create a feature char_count from "text" feature. Use len function to count
-tweets['char_count'] = tweets[---].apply(--)
+tweets['char_count'] = tweets['text'].apply(len)
 
 # Print the average character count
-print("char count mean: ",tweets[----].---)
+print("Char count mean: ", tweets['char_count'].mean())
 
 
 ## HASHTAGS COUNT
 
-# Function that returns numner of hashtags in a string
+# Function that returns number of hashtags in a string
 def count_hashtags(string):
     # Split the string into words
     words = string.split()
 
     # Create a list of words that are hashtags
-    hashtags = [word for word in words if ____.____(____)]
+    hashtags = [word for word in words if word.startswith('#')]
 
     # Return number of hashtags
-    return (len(hashtags))
+    return len(hashtags)
 
 
 # Create a feature hashtag_count and display distribution
 tweets['hashtag_count'] = tweets['text'].apply(count_hashtags)
 tweets['hashtag_count'].hist()
 plt.title('Hashtag count distribution')
+plt.xlabel('Number of Hashtags')
+plt.ylabel('Frequency')
 plt.show()
 
+
+## MENTIONS COUNT
 
 # Function that returns number of mentions in a string
 def count_mentions(string):
@@ -57,14 +62,16 @@ def count_mentions(string):
     words = string.split()
 
     # Create a list of words that are mentions
-    mentions = [word for word in words if ____.____(____)]
+    mentions = [word for word in words if word.startswith('@')]
 
     # Return number of mentions
-    return (len(mentions))
+    return len(mentions)
 
 
 # Create a feature mention_count and display distribution
 tweets['mention_count'] = tweets['text'].apply(count_mentions)
 tweets['mention_count'].hist()
 plt.title('Mention count distribution')
+plt.xlabel('Number of Mentions')
+plt.ylabel('Frequency')
 plt.show()
