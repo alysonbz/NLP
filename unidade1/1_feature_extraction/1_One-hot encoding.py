2cc15e9e3a301e5536d30c7cd881ef89dd8502a4
João Barbosa <davibarbosabdj@alu.ufc.br>
2024-07-25 13:49:10 -0300

    add new exercise

--- unidade1/1_feature_extraction/1_One-hot encoding.py
+++ unidade1/1_feature_extraction/1_One-hot encoding.py
@@ -1,4 +1,6 @@
+import pandas as pd
 
+df1 = pd.read_csv(r'C:\Users\joaod\Documents\faculdade-ciencia-de-dados\Semestres\2024_1\NLP\unidade1\dataset\df1_unidade1.csv')
 
 # Print the features of df1
 print(df1.columns)
