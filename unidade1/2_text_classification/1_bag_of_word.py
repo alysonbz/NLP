dac195123d5fcd53134cd6f0cac6101888d1b1a9
Larissa Vitória <larissavitory33@gmail.com>
2024-07-26 13:05:26 -0300

    NLP - aula 26/07/24

--- unidade1/2_text_classification/1_bag_of_word.py
+++ unidade1/2_text_classification/1_bag_of_word.py
@@ -1,16 +1,16 @@
 from src.utils import load_movie_review_dataset
 
 # Import CountVectorizer
-from sklearn.feature_extraction.text import ____
+from sklearn.feature_extraction.text import CountVectorizer
 
 corpus = load_movie_review_dataset()
 corpus = corpus['tagline'].dropna()
 
 # Create CountVectorizer object
-____ = ____
+vectorizer = CountVectorizer(strip_accents='ascii', stop_words='english', lowercase=False)
 
 # Generate matrix of word vectors
-bow_matrix = vectorizer.____(____)
+bow_matrix = vectorizer.fit_transform(corpus)
 
 # Print the shape of bow_matrix
 print(bow_matrix.shape)
