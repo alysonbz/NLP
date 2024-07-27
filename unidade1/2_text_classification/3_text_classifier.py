6ffc1ec9642da1c925c58bca2a9b6df9090b468a
Mateus Silva <mateussilvamatos@alu.ufc.br>
2024-07-27 10:44:07 -0300

    atividades 25/07

--- unidade1/2_text_classification/3_text_classifier.py
+++ unidade1/2_text_classification/3_text_classifier.py
@@ -4,33 +4,35 @@ from sklearn.model_selection import train_test_split
 from src.utils import load_movie_review_clean_dataset
 from sklearn.naive_bayes import MultinomialNB
 
+# Load dataset
 corpus = load_movie_review_clean_dataset()
 X = corpus['review']
 y = corpus['sentiment']
-X_train,  X_test,y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=42)
 
-# Create a CountVectorizer object
-vectorizer = ____(lowercase=____, stop_words=____)
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Create a CountVectorizer object with specified parameters
+vectorizer = CountVectorizer(lowercase=True, stop_words='english')
 
 # Fit and transform X_train
-X_train_bow = vectorizer.____(____)
+X_train_bow = vectorizer.fit_transform(X_train)
 
 # Transform X_test
-X_test_bow = vectorizer.____(____)
+X_test_bow = vectorizer.transform(X_test)
 
 # Print shape of X_train_bow and X_test_bow
-print(X_train_bow.shape)
-print(X_test_bow.shape)
-
+print("Shape of X_train_bow:", X_train_bow.shape)
+print("Shape of X_test_bow:", X_test_bow.shape)
 
 # Create a MultinomialNB object
-clf = ____
+clf = MultinomialNB()
 
-# Fit the classifier
-clf.____(____, ____)
+# Fit the classifier on the training data
+clf.fit(X_train_bow, y_train)
 
-# Measure the accuracy
-accuracy = clf.score(____, ____)
+# Measure the accuracy on the test data
+accuracy = clf.score(X_test_bow, y_test)
 print("The accuracy of the classifier on the test set is %.3f" % accuracy)
 
 # Predict the sentiment of a negative review
