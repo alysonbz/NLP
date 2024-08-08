b618cae351ffd495fa7ba51d8ccd87a64f20ee95
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2024-08-08 12:37:10 -0300

    unidade2

--- unidade1/2_text_classification/1_bag_of_word.py
+++ unidade1/2_text_classification/1_bag_of_word.py
@@ -7,10 +7,10 @@ corpus = load_movie_review_dataset()
 corpus = corpus['tagline'].dropna()
 
 # Create CountVectorizer object
-____ = ____
+vectorizer = ____
 
 # Generate matrix of word vectors
-bow_matrix = vectorizer.____(____)
+bow_matrix = _____
 
 # Print the shape of bow_matrix
 print(bow_matrix.shape)
--- unidade1/2_text_classification/3_text_classifier.py
+++ unidade1/2_text_classification/3_text_classifier.py
@@ -7,7 +7,7 @@ from sklearn.naive_bayes import MultinomialNB
 corpus = load_movie_review_clean_dataset()
 X = corpus['review']
 y = corpus['sentiment']
-X_train,  X_test,y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=42)
+X_train,  X_test,y_train, y_test = _____
 
 # Create a CountVectorizer object
 vectorizer = ____(lowercase=____, stop_words=____)
