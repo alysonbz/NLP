570436744f09eef0d328cbc392c5c930b8b6fd22
Pedro <filhopedro465@gmail.com>
2024-08-08 10:37:38 -0300

    dia 01/02

--- unidade1/2_text_classification/1_bag_of_word.py
+++ unidade1/2_text_classification/1_bag_of_word.py
@@ -1,16 +1,17 @@
 from src.utils import load_movie_review_dataset
 
 # Import CountVectorizer
-from sklearn.feature_extraction.text import ____
+from sklearn.feature_extraction.text import CountVectorizer
 
+# Load the dataset
 corpus = load_movie_review_dataset()
 corpus = corpus['tagline'].dropna()
 
 # Create CountVectorizer object
-____ = ____
+vectorizer = CountVectorizer()
 
 # Generate matrix of word vectors
-bow_matrix = vectorizer.____(____)
+bow_matrix = vectorizer.fit_transform(corpus)
 
 # Print the shape of bow_matrix
 print(bow_matrix.shape)
