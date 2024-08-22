879e20cc1cdf1bd08d3c15a10e053cbea95a3aa0
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2024-08-22 13:41:18 -0300

    LLM

---
+++ unidade2/1_llm_introduction/1_sentiment_classifier.py
@@ -0,0 +1,12 @@
+# Import the function for loading Hugging Face pipelines
+
+
+prompt = "The food was good, but service at the restaurant was a bit slow"
+
+# Load the pipeline for sentiment classification
+classifier =
+
+# Pass the customer review to the model for prediction
+prediction =
+
+print(prediction)
