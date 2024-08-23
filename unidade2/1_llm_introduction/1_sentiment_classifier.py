c5d78d822c5b2959353423355379b0a62e4500c2
Larissa Vitória <larissavitory33@gmail.com>
2024-08-23 14:53:52 -0300

    NLP - Aulas 22 e 23 ago, 2024.

--- unidade2/1_llm_introduction/1_sentiment_classifier.py
+++ unidade2/1_llm_introduction/1_sentiment_classifier.py
@@ -1,12 +1,14 @@
 # Import the function for loading Hugging Face pipelines
-
+from transformers import pipeline
 
 prompt = "The food was good, but service at the restaurant was a bit slow"
 
 # Load the pipeline for sentiment classification
-classifier =
+classifier = pipeline("text-classification", model="nlptown/bert-base-multilingual-uncased-sentiment")
 
 # Pass the customer review to the model for prediction
-prediction =
-
+prediction = classifier(prompt)
 print(prediction)
+
+
+#instalar = tensorflow 2.3 keras 2.4
