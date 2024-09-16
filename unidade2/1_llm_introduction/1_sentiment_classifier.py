dfc2e7d88d9fac0efe20aa72516ce22c555855da
Mateus Silva <mateussilvamatos@alu.ufc.br>
2024-09-15 22:25:39 -0300

    exercicios

--- unidade2/1_llm_introduction/1_sentiment_classifier.py
+++ unidade2/1_llm_introduction/1_sentiment_classifier.py
@@ -1,12 +1,12 @@
-# Import the function for loading Hugging Face pipelines
+from transformers import pipeline
 
-
+# Texto de exemplo
 prompt = "A comida foi adequada. O serviço do garçom foi lento."
 
-# Load the pipeline for sentiment classification
-classifier =
+# Carregar o pipeline para classificação de sentimento
+classifier = pipeline('sentiment-analysis')
 
-# Pass the customer review to the model for prediction
-prediction =
+# Passar a revisão do cliente para o modelo para previsão
+prediction = classifier(prompt)
 
 print(prediction)
