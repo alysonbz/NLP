f0be18ffd9deec9980ab93425e9ecb10ae2e76c1
Larissa Vitória <larissavitory33@gmail.com>
2024-09-21 16:01:09 -0300

    Melhorias na AV1 e realização da AV2 no Colab, em 21/09/2024.

--- AV1/preprocessing.py
+++ AV1/preprocessing.py
@@ -37,6 +37,11 @@ print("\nNulos:", nulos)
 df = df.dropna(subset=['content'])
 print(f"Número de linhas após modificação: {len(df)}")
 
+df = df[df['score'] != 3]  # Removendo as linhas onde o score é igual a 3
+
+df['score'] = df['score'].replace({1: 'Ruim', 2: 'Ruim', 4: 'Bom', 5: 'Bom'})  # transformando classes em somente bom e ruim
+df['score'] = df['score'].replace({'Ruim': 0, 'Bom': 1}) # mapeando
+
 # Extração e balanceamento das classes (amostra de 10.000 linhas)
 desired_count = 10000 // len(df['score'].value_counts())
 rus = RandomUnderSampler(sampling_strategy={score: desired_count for score in df['score'].unique()}, random_state=1)
@@ -150,3 +155,66 @@ print(df[['content', 'content_lemmatized', 'content_stemmed']].head())
 
 colunas_content = ['id','content', 'content_stemmed', 'content_lemmatized', 'score']
 df[colunas_content].to_csv('content_lemmatized_stemmed.csv', index=False)
+
+
+
+
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+from wordcloud import WordCloud
+
+def plot_most_common_words(words, title):
+  word_counts = Counter(words)
+
+    # Obtém as 10 palavras mais comuns
+  most_common = word_counts.most_common(10)
+
+    # Cria um DataFrame para o gráfico
+  word_freq_df = pd.DataFrame(most_common, columns=['Word', 'Frequency'])
+
+    # Cria o gráfico de barras
+  plt.figure(figsize=(10, 6))
+  plt.bar(word_freq_df['Word'], word_freq_df['Frequency'], color='pink')
+  plt.xlabel('Word')
+  plt.ylabel('Frequency')
+  plt.title(title)
+  plt.xticks(rotation=45)
+  plt.show()
+
+def nuvem_palavras(words, title):
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(' '.join(words))
+
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.title(title, fontsize=16)
+    plt.show()
+
+# Para a coluna 'content' (texto bruto)
+content_words = ' '.join(df['content']).split()
+plot_most_common_words(content_words, '10 palavras mais frequentes - Avaliações originais')
+
+# Para a coluna 'content_lemmatized'
+lemmatized_words = [word for sublist in df['content_lemmatized'] for word in sublist]
+plot_most_common_words(lemmatized_words, '10 palavras mais frequentes - Lemmatized')
+nuvem_palavras(lemmatized_words, 'Nuvem de Palavras - Lemmatized')
+
+# Para a coluna 'content_stemmed'
+stemmed_words = [word for sublist in df['content_stemmed'] for word in sublist]
+plot_most_common_words(stemmed_words, '10 palavras mais frequentes - Stemmed')
+
+# Contagem dos valores na coluna "score"
+score_counts = df['score'].value_counts().sort_index()
+
+# Criando o gráfico de barras
+plt.figure(figsize=(8, 6))
+score_counts.plot(kind='bar', color='pink')
+plt.title('Distribuição das Avaliações (Score)')
+plt.xlabel('Score (Estrelas)')
+plt.ylabel('Frequência')
+plt.xticks(rotation=0)
+plt.show()
