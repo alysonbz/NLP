c4b8d6a3a416a3c9630e6002d00e4c443642a57b
Sheldaa <sheldasouza@alu.ufc.br>
2024-12-09 11:18:00 -0300

    AV1 e atividades que faltam

--- unidade1/2_featrure_extraction_frequency_based/5_cooc_matrix_for_classification.py
+++ unidade1/2_featrure_extraction_frequency_based/5_cooc_matrix_for_classification.py
@@ -8,5 +8,8 @@ corpus = load_movie_review_clean_dataset()
 X = corpus['review']
 y = corpus['sentiment']
 
-X_train,  X_test,y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=42)
+vocabulary = list(set(" ".join(X).split()))
+main_dic = {word: idx for idx, word in enumerate(vocabulary)}
+print(main_dic)
 
+X_train,  X_test,y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=42)
