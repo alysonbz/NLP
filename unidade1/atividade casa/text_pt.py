cdbc6913f5fac45d15b1dc4939736fb03324bc41
Luciana <lucianasmartins779@gmail.com>
2024-12-09 22:18:24 -0300

    .

--- unidade1/atividade casa/text_pt.py
+++ unidade1/atividade casa/text_pt.py
@@ -3,3 +3,39 @@
 # b) Tokenize o texto
 # c) Remova as stop words
 # d) Faça uma tabela mostrando 3 colunas para todos os tokens: token , stemming e lematização
+
+import spacy
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import RSLPStemmer
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('rslp')
+
+nlp = spacy.load('pt_core_news_sm')
+stop_words = set(stopwords.words('portuguese'))
+stemmer = RSLPStemmer()
+
+
+def process_text(text):
+    doc = nlp(text)
+    corrected_text = " ".join([token.text for token in doc])
+    tokens = word_tokenize(corrected_text, language='portuguese')
+    filtered_tokens = [token for token in tokens if token.lower() not in stop_words and token.isalpha()]
+
+    data = []
+    for token in filtered_tokens:
+        stem = stemmer.stem(token)
+        lemma = nlp(token)[0].lemma_
+        data.append([token, stem, lemma])
+
+    df = pd.DataFrame(data, columns=["Token", "Stemming", "Lemmatization"])
+    return df
+
+
+text = "Os menino estão estudando muito no escola. Eles quer aprender."
+df = process_text(text)
+print(df)
