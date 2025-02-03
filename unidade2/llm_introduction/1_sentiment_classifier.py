cf9008e3239dd88773b528c7fe96d0e09b6f17ca
Emilymendc <emilymendc@alu.ufc.br>
2025-02-03 15:01:20 -0300

    Atualização

--- unidade2/llm_introduction/1_sentiment_classifier.py
+++ unidade2/llm_introduction/1_sentiment_classifier.py
@@ -1,12 +1,13 @@
 # Import the function for loading Hugging Face pipelines
-
+from transformers import pipeline
 
 prompt = "A comida foi adequada. O serviço do garçom foi lento."
 
 # Load the pipeline for sentiment classification
-classifier =
+classifier = pipeline(task="text-classification",
+                      model="nlptown/bert-base-multilingual-uncased-sentiment")
 
 # Pass the customer review to the model for prediction
-prediction =
+prediction = classifier(prompt)
 
 print(prediction)
