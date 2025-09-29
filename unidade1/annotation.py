a79b48bab54f39c16e1f1db98134a43f39a36d79
jeftedamaceno <jeftedamaceno@gmail.com>
2025-09-29 16:43:34 -0300

    add annotation.py

---
+++ unidade1/annotation.py
@@ -0,0 +1,25 @@
+import spacy
+from spacy.lang.pt.examples import sentences
+
+nlp = spacy.load("pt_core_news_sm")
+
+for s in sentences:
+    print(f'\n {s}')
+
+doc = nlp(sentences[0])
+
+print("\nTokens")
+for token in doc:
+    print(token.text)
+
+print("\nLemmas")
+for token in doc:
+    print(token.text, token.lemma_)
+
+print("\nNER - Name Entity Recognition")
+for ent in doc.ents:
+    print(ent.text, ent.label_)
+
+print("\n - Part Of Speech Tagging")
+for token in doc:
+    print(token.text, token.pos_)
