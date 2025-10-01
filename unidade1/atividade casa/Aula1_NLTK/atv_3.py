74251ca053eade225be8d302c33d243cf3e4bf92
Leandro Adegas <ecommerce.adegas@gmail.com>
2025-10-01 11:23:38 -0300

    .

--- unidade1/atividade casa/Aula1_NLTK/atv_3.py
+++ unidade1/atividade casa/Aula1_NLTK/atv_3.py
@@ -10,39 +10,41 @@ from nltk.stem import RSLPStemmer
 import pandas as pd
 import spacy
 from enelvo.normaliser import Normaliser
+
 nlp = spacy.load("pt_core_news_sm")
 #nltk.download('rslp')  # rodar uma vez
 stemmer = RSLPStemmer()   # somente para deixar o stemming em português
+
+
 # Corretor
 normalizador = Normaliser(tokenizer='readable', capitalize_pns = True, capitalize_acs = True, capitalize_inis = True, sanitize = True)
+
 msg = 'a maria foi ao shopp pq estava trsite, acho q hj foi uma dia dificiu.'
 resposta = normalizador.normalise(msg)
 print('\nTexto corrigido:', resposta)
+
+
 doc = nlp(resposta)     # pegando já corrigida
+
 # Token
 tk = []
 for token in doc:
     tk.append(token.text)
+
+
 # Stemming
 stm = [stemmer.stem(token) for token in tk]
 stem = []
 for token in stm:
     stem.append(token)
+
+
 # Lemmatização
 lem = []
 for token in doc:
     lem.append(token.lemma_)
+
+
 # Tabela
 df = pd.DataFrame({'token': tk, 'stemming': stem, 'lemma': lem})
 print('\n',df)
-
-
-
-
-
-
-
-
-
-
-
