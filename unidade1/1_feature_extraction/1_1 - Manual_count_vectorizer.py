2e7951ad94c5ee989ca8902730e582560e55c646
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2025-10-20 16:01:58 -0300

    nlp

--- unidade1/1_feature_extraction/1_1 - Manual_count_vectorizer.py
+++ unidade1/1_feature_extraction/1_1 - Manual_count_vectorizer.py
@@ -1,11 +1,15 @@
 import pandas as pd
-from src.utils import load_df1_one_hot
+from src.utils import load_movie_review_clean_dataset
 
-df1 = load_df1_one_hot()
+df = load_movie_review_clean_dataset()
 
 
 def count_vectorizer(list_to_vect):
+    #tokenizar
+    #criar saco de palavra (id para cada termo)
+    #criar a matriz de dicionario para cada sentença
     #seu código aqui
     return None
 
-print(count_vectorizer(df1['feature 5']))
+print(df['review'].head(1))
+print(count_vectorizer(df['review']))
