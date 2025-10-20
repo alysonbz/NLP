8a9961722fd5f8317616f09bf77de709140779a8
MarioJOP <mariopatricio599@gmail.com>
2025-10-20 19:45:13 -0300

    adiciona atividade count vectorizer

--- unidade1/1_feature_extraction/2_atributes_extraction.py
+++ unidade1/1_feature_extraction/2_atributes_extraction.py
@@ -1,3 +1,7 @@
+import pandas as pd
+from src.utils import load_movie_review_clean_dataset
+
+
 # Atividade
 # 1) Um função que retorne a quantidade de sentenças em um texto.
 def count_sentences(text: str):
@@ -5,7 +9,7 @@ def count_sentences(text: str):
 
 # 2) Uma função que retorne a quantidade de palavras que começam com letra maiúscula em um texto.
 def count_capitalized_words(text: str):
-    return len([word for word in text.split() if word.istitle()])
+    return len([word for word in text.split() if word[0] == word[0].upper()])
 
 # 3) Uma função que retorne a quantidade de caracteres numéricos em um texto.
 def count_numeric_characters(text: str):
@@ -16,4 +20,11 @@ def count_uppercase_words(text: str):
     return len([word for word in text.split() if word.isupper()])
 
 # 5) Crie um dataframe de 1 coluna e 4 linhas. Este contém os textos testados nas questões anteriores.
+df = load_movie_review_clean_dataset()
 # 6) Crie e print um dataframe de 4 linhas e 4 colunas e preencha com a execução das funções das questões 1,2,3 e 4.  Cada coluna deve conter os resultados das funções. Utilize a função apply() no dataframe criado na questão 5.
+df['count_sentences_review'] = df['review'].apply(count_sentences)
+df['count_capitalized_review'] = df['review'].apply(count_capitalized_words)
+df['count_numeric_review'] = df['review'].apply(count_numeric_characters)
+df['count_uppercase_review'] = df['review'].apply(count_uppercase_words)
+
+print(df.head())
