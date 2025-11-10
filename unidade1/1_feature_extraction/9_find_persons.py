d0729f932401f310f36dd592abe892e3ecc0deb9
Leandro Adegas <ecommerce.adegas@gmail.com>
2025-11-10 16:57:10 -0300

    dia10_11

--- unidade1/1_feature_extraction/9_find_persons.py
+++ unidade1/1_feature_extraction/9_find_persons.py
@@ -2,6 +2,7 @@ import spacy
 from src.utils import load_text_tc
 
 tc = load_text_tc()
+print(tc)
 
 # Load model and create Doc object
 nlp = spacy.load('en_core_web_sm')
@@ -9,13 +10,13 @@ nlp = spacy.load('en_core_web_sm')
 
 def find_persons(text):
     # Create Doc object
-    doc = ___(___)
+    doc = nlp(text)
 
     # Identify the persons
-    persons = [ent.____ for ent in doc.____ if ent.____ == 'PERSON']
+    persons = [ent.text for ent in doc.ents if ent.label_ == 'PERSON']
 
     # Return persons
     return persons
 
 
-print(____(____))
+print(find_persons(tc))
